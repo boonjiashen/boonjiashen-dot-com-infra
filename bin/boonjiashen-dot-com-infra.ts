2a5d67292a3afeeff3d07fa60921dae0e7ad9c00
Jia-Shen Boon <jiashen@gmail.com>
2021-09-05 09:57:04 +0900

    Manage prod stack with CDK
    Previously it was manually created
    __Testing done__
    
    1. Delete manually created boonjiashen.com bucket
    1. `cdk deploy infraStackProd` succeeds
    1. <http://boonjiashen.com.s3-website-ap-northeast-1.amazonaws.com> redirects to <https://blog.boonjiashen.com/>
    1. Changed named servers in Route53 registrar to those in CDK-managed hosted zone (to be seen when boonjiashen.com redirects to blog.boonjiashen.com)
    1. See that <https://s3.ap-northeast-1.amazonaws.com/boonjiashen.com/resume.pdf> directs to resume

--- bin/boonjiashen-dot-com-infra.ts
+++ bin/boonjiashen-dot-com-infra.ts
@@ -4,7 +4,14 @@ import * as cdk from 'monocdk';
 import {InfraStack} from '../lib/infra-stack';
 
 const app = new cdk.App();
+
 new InfraStack(app, 'infraStackBeta', {
   topLevelDomainName: 'boonjiashen-dev.com',
   githubUsername: 'boonjiashen2',
+});
+
+new InfraStack(app, 'infraStackProd', {
+  topLevelDomainName: 'boonjiashen.com',
+  githubUsername: 'boonjiashen',
+  domainVerificationToken: "google-site-verification=vdXn5ouve-i3MgKncs1Q2I_t_T1IUrkVtLojnNuBpV8",
 });
